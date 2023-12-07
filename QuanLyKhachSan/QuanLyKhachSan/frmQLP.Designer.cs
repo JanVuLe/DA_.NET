42a7c72c5c1ef4cf2b3af7899ba6fee742bab61f
Le Van Vu <lvljanv@gmail.com>
2023-12-07 11:21:53 +0700

    lv

--- QuanLyKhachSan/QuanLyKhachSan/frmQLP.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/frmQLP.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(272, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 135);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LV";
+            // 
+            // frmQLP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "frmQLP";
             this.Text = "frmQLP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
